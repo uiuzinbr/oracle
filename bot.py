00d2ac7a50031a68d6232bad9c46421ed81ba733
uiuzinbr <123005043+uiuzinbr@users.noreply.github.com>
2023-01-18 21:59:15 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,137 @@
+displayName = 'cloudlands'
+compartmentId = 'ocid1.tenancy.oc1..aaaaaaaawfihzxdgchstlcppa4rhv3uxzyn7loaza5vw6e5rihnx62m66wqa'
+availabilityDomain = "zQmO:SA-SAOPAULO-1-AD-1"
+imageId = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaaregfux63u3c3u75f3sfegclcgbyfwqpprwzja7t3d43zstumybpa"
+subnetId = 'ocid1.subnet.oc1.sa-saopaulo-1.aaaaaaaagk47iwsksjcogiylbfuxqhssa3q74ophhpsdb46fm4x7mctsrgwa'
+ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCazBvOJGeiQVw7zbRNt01V9WG4Xtj6Ql5MmSjM5FFRzFslEQRNkoz43AIMHwnmgZd303ZR8VFMu2TGKxs+8wV++LOT77byM76DZbVm/MNG+G2XkAreoKrfW3ru4e21r4z7Rz464n8yPyMxEBFk29sFUifRs7dXRPLwMHsoYM3BCcwpfEIWycCZJZ6XSM+r07g93AEhJGao8sXdI7ZI+a0VBwFxBSlaQ8l/tRROIeTfYykHrrqcvUYP+43GdOQkASFaVWnHDD8v2bfjr6A5MuRAm9475ic5ikJanp+PyY4BELyBiNWJE1aFhqgMmKHg+xGysRDDGCZiqOjffVMqPWaL ssh-key-2023-01-19"
+
+import os
+os.system("pip install oci")
+os.system("pip install requests")
+import oci
+import logging
+import time
+import sys
+import requests
+
+LOG_FORMAT = '[%(levelname)s] %(asctime)s - %(message)s'
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOG_FORMAT,
+    handlers=[
+        logging.FileHandler("oci.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+ocpus = 2
+memory_in_gbs = 12
+wait_s_for_retry = 10
+
+logging.info("#####################################################")
+logging.info("Script to spawn VM.Standard.A1.Flex instance")
+
+
+message = f'Start spawning instance VM.Standard.A1.Flex - {ocpus} ocpus - {memory_in_gbs} GB'
+logging.info(message)
+
+logging.info("Loading OCI config")
+config = oci.config.from_file(file_location="./config")
+
+logging.info("Initialize service client with default config file")
+to_launch_instance = oci.core.ComputeClient(config)
+
+
+message = f"Instance to create: VM.Standard.A1.Flex - {ocpus} ocpus - {memory_in_gbs} GB"
+logging.info(message)
+
+logging.info("Check current instances in account")
+logging.info(
+    "Note: Free upto 4xVM.Standard.A1.Flex instance, total of 4 ocpus and 24 GB of memory")
+current_instance = to_launch_instance.list_instances(
+    compartment_id=compartmentId)
+response = current_instance.data
+
+total_ocpus = total_memory = _A1_Flex = 0
+instance_names = []
+if response:
+    logging.info(f"{len(response)} instance(s) found!")
+    for instance in response:
+        logging.info(f"{instance.display_name} - {instance.shape} - {int(instance.shape_config.ocpus)} ocpu(s) - {instance.shape_config.memory_in_gbs} GB(s) | State: {instance.lifecycle_state}")
+        instance_names.append(instance.display_name)
+        if instance.shape == "VM.Standard.A1.Flex" and instance.lifecycle_state not in ("TERMINATING", "TERMINATED"):
+            _A1_Flex += 1
+            total_ocpus += int(instance.shape_config.ocpus)
+            total_memory += int(instance.shape_config.memory_in_gbs)
+
+    message = f"Current: {_A1_Flex} active VM.Standard.A1.Flex instance(s) (including RUNNING OR STOPPED)"
+    logging.info(message)
+else:
+    logging.info(f"No instance(s) found!")
+
+
+message = f"Total ocpus: {total_ocpus} - Total memory: {total_memory} (GB) || Free {4-total_ocpus} ocpus - Free memory: {24-total_memory} (GB)"
+logging.info(message)
+
+if total_ocpus + ocpus > 4 or total_memory + memory_in_gbs > 24:
+    message = "Total maximum resource exceed free tier limit (Over 4 ocpus/24GB total). **SCRIPT STOPPED**"
+    logging.critical(message)
+    sys.exit()
+
+if displayName in instance_names:
+    message = f"Duplicate display name: >>>{displayName}<<< Change this! **SCRIPT STOPPED**"
+    logging.critical(message)
+    sys.exit()
+
+message = f"Precheck pass! Create new instance VM.Standard.A1.Flex: {ocpus} opus - {memory_in_gbs} GB"
+logging.info(message)
+
+instance_detail = oci.core.models.LaunchInstanceDetails(
+    metadata={
+        "ssh_authorized_keys": ssh_authorized_keys
+    },
+    availability_domain=availabilityDomain,
+    shape='VM.Standard.A1.Flex',
+    compartment_id=compartmentId,
+    display_name=displayName,
+    source_details=oci.core.models.InstanceSourceViaImageDetails(
+        source_type="image", image_id=imageId),
+    create_vnic_details=oci.core.models.CreateVnicDetails(
+        assign_public_ip=False, subnet_id=subnetId, assign_private_dns_record=True),
+    agent_config=oci.core.models.LaunchInstanceAgentConfigDetails(
+        is_monitoring_disabled=False,
+        is_management_disabled=False,
+        plugins_config=[oci.core.models.InstanceAgentPluginConfigDetails(
+            name='Vulnerability Scanning', desired_state='DISABLED'), oci.core.models.InstanceAgentPluginConfigDetails(name='Compute Instance Monitoring', desired_state='ENABLED'), oci.core.models.InstanceAgentPluginConfigDetails(name='Bastion', desired_state='DISABLED')]
+    ),
+    defined_tags={},
+    freeform_tags={},
+    instance_options=oci.core.models.InstanceOptions(
+        are_legacy_imds_endpoints_disabled=False),
+    availability_config=oci.core.models.LaunchInstanceAvailabilityConfigDetails(
+        recovery_action="RESTORE_INSTANCE"),
+    shape_config=oci.core.models.LaunchInstanceShapeConfigDetails(
+        ocpus=ocpus, memory_in_gbs=memory_in_gbs)
+)
+
+to_try = True
+while to_try:
+    try:
+        to_launch_instance.launch_instance(instance_detail)
+        to_try = False
+        message = 'Success! Edit vnic to get public ip address'
+        logging.info(message)
+        sys.exit()
+    except oci.exceptions.ServiceError as e:
+        if e.status == 500:
+            message = f"{e.message} Retry in {wait_s_for_retry}s"
+        else:
+            message = f"{e} Retry in {wait_s_for_retry}s"
+        logging.info(message)
+        time.sleep(wait_s_for_retry)
+    except Exception as e:
+        message = f"{e} Retry in {wait_s_for_retry}s"
+        logging.info(message)
+        time.sleep(wait_s_for_retry)
+    except KeyboardInterrupt:
+        sys.exit()
